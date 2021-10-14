54ab722d6f20e8897403493d3497c61205a8622c
Aidansieg <1252Riverbayroad>
2021-10-14 15:13:54 -0400

    Added so many things. Basically, this should produce a white screen with a button that allows the user to login with their spotify account but the IDE is telling me that it can't find the "SpotifyPlaylistGenerator-Bridging-Header.h" file even though (I think) its in the correct location. We also probably need to set up some sort of backend portion using UVM's Silk Server.

--- Spotify Playlist Generator/Spotify Playlist Generator/AppDelegate.swift
+++ Spotify Playlist Generator/Spotify Playlist Generator/AppDelegate.swift
@@ -1,37 +1,33 @@
-//
-//  AppDelegate.swift
-//  Spotify Playlist Generator
-//
-//  Created by Aidan Siegel on 10/12/21.
-//
-
-//test
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
+    lazy var rootViewController = ViewController()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        rootViewController.sessionManager.application(app, open: url, options: options)
+        return true
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func applicationWillResignActive(_ application: UIApplication) {
+        if (rootViewController.appRemote.isConnected) {
+            rootViewController.appRemote.disconnect()
+        }
     }
 
-
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        if let _ = rootViewController.appRemote.connectionParameters.accessToken {
+            rootViewController.appRemote.connect()
+        }
+    }
 }
 
---
+++ Spotify Playlist Generator/Spotify Playlist Generator/SpotifyPlaylistGenerator-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  SpotifyPlaylistGenerator-Bridging-Header.h
+//  Spotify Playlist Generator
+//
+//  Created by Aidan Siegel on 10/14/21.
+//
+
+#import <SpotifyiOS/SpotifyiOS.h>
